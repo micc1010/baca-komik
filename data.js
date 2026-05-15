8ca9d4b34dd486f3a0a543cd9ff22b1ab0bc20a8
micc1010 <michaellong23.ml@gmail.com>
2026-05-15 11:01:17 +0000

    new7

--- data.js
+++ data.js
@@ -14,7 +14,21 @@ const comics = [
                     "https://i.imgur.com/IHsfaWQ.png",
                     "https://i.imgur.com/a8olAmo.png",
                     "https://i.imgur.com/eI0xkkZ.png",
-                    "https://i.imgur.com/BeEnPCp.png"
+                    "https://i.imgur.com/BeEnPCp.png",
+                    "https://i.imgur.com/pdbSz4l.png",
+                    "https://i.imgur.com/rVoONxF.png",
+                    "https://i.imgur.com/L9QFUC5.png",
+                    "https://i.imgur.com/xzOSIcA.png",
+                    "https://i.imgur.com/7RbWP7V.png",
+                    "https://i.imgur.com/BgWMiQg.png",
+                    "https://i.imgur.com/vyx2SCb.png",
+                    "https://i.imgur.com/wT9RrJg.png",
+                    "https://i.imgur.com/H0g7OBR.png",
+                    "https://i.imgur.com/w8RZfMq.png",
+                    "https://i.imgur.com/gkl2fzO.png",
+                    "https://i.imgur.com/C12dQaq.png",
+                    "https://i.imgur.com/hJPTacF.png",
+                    "https://i.imgur.com/Gc2P9IZ.png"
                 ]
             },
             {
