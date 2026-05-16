0a1b19ff42227c2eb1632e1c0e69e54090c40520
micc1010 <michaellong23.ml@gmail.com>
2026-05-16 08:26:47 +0700

    new6

--- data.js
+++ data.js
@@ -2,9 +2,9 @@
 const comics = [
     {
         id: 1,
-        title: "One Piece Remastered",
-        poster: "https://images.unsplash.com/photo-1607604276583-eef5d076aa5f?w=400",
-        desc: "Kisah petualangan Monkey D. Luffy untuk menjadi Raja Bajak Laut di seluruh lautan.",
+        title: "One Punch Man",
+        poster: "https://i.imgur.com/t9JWnT4.png",
+        desc: "One Punch Man mengikuti kisah Saitama, seorang pahlawan yang bisa mengalahkan musuh hanya dengan satu pukulan. Meskipun kekuatannya luar biasa, ia merasa bosan dan kesepian karena tidak ada lawan yang menantang. Petualangannya dimulai saat ia bergabung dengan Asosiasi Pahlawan dan bertemu dengan berbagai pahlawan dan monster yang unik. Dalam perjalanannya, Saitama harus berhadapan dengan ancaman besar yang mengancam dunia. Meskipun sering diremehkan oleh rekan-rekannya, ia terus berusaha melindungi warga dan mencari arti sesungguhnya dari menjadi seorang pahlawan. Cerita ini menyajikan perpaduan antara aksi seru dan humor yang segar.",
         direct: "https://google.com", // MASUKKAN DIRECT LINK ADSTERRA ANDA
         chapters: [
             {
@@ -41,6 +41,49 @@ const comics = [
             }
             // Jika ingin tambah Chapter 3, tinggal copas struktur di atas dan taruh di bawah sini
         ]
-    }
+    },
+
+    {
+        id: 2,
+        title: "Solo Leveling",
+        poster: "https://i.imgur.com/xKVmzM3.png",
+        desc: "Di dunia di mana para pemburu bertarung melawan makhluk dari dunia lain melalui portal misterius, Sung Jinwoo adalah pemburu dengan kekuatan yang sangat lemah. Sebagai pemburu kelas terendah, ia sering dianggap tidak berguna dan sering menghadapi bahaya yang mengancam nyawanya dalam setiap misi. Namun, semuanya berubah ketika ia mendapatkan kemampuan khusus yang memungkinkannya untuk naik level secara mandiri dan menjadi semakin kuat tanpa batas. Dengan kekuatannya yang terus berkembang, Jinwoo mulai mengeksplorasi berbagai dungeon berbahaya dan menghadapi monster-monster menakutkan yang bahkan tidak bisa ditaklukkan oleh pemburu lain. Perjalanannya untuk menjadi pemburu terkuat membawa banyak tantangan serta intrik, termasuk menghadapi organisasi rahasia dan ancaman yang lebih besar dari dunia lain. Kisah ini mengeksplorasi perjuangan, pengorbanan, dan perubahan Jinwoo yang luar biasa di dunia penuh ancaman tersebut.",
+        direct: "https://google.com", // MASUKKAN DIRECT LINK ADSTERRA ANDA
+        chapters: [
+            {
+                chNum: 1,
+                title: "Ch. 01",
+                images: [
+                    "https://i.imgur.com/IHsfaWQ.png",
+                    "https://i.imgur.com/a8olAmo.png",
+                    "https://i.imgur.com/eI0xkkZ.png",
+                    "https://i.imgur.com/BeEnPCp.png",
+                    "https://i.imgur.com/pdbSz4l.png",
+                    "https://i.imgur.com/rVoONxF.png",
+                    "https://i.imgur.com/L9QFUC5.png",
+                    "https://i.imgur.com/xzOSIcA.png",
+                    "https://i.imgur.com/7RbWP7V.png",
+                    "https://i.imgur.com/BgWMiQg.png",
+                    "https://i.imgur.com/vyx2SCb.png",
+                    "https://i.imgur.com/wT9RrJg.png",
+                    "https://i.imgur.com/H0g7OBR.png",
+                    "https://i.imgur.com/w8RZfMq.png",
+                    "https://i.imgur.com/gkl2fzO.png",
+                    "https://i.imgur.com/C12dQaq.png",
+                    "https://i.imgur.com/hJPTacF.png",
+                    "https://i.imgur.com/Gc2P9IZ.png"
+                ]
+            },
+            {
+                chNum: 2,
+                title: "Ch. 02: Luffy Topi Jerami",
+                images: [
+                    "https://images.unsplash.com/photo-1614850523060-8da1d56ae167?w=700",
+                    "https://images.unsplash.com/photo-1618005198143-e5283b519a7f?w=700"
+                ]
+            }
+            // Jika ingin tambah Chapter 3, tinggal copas struktur di atas dan taruh di bawah sini
+        ]
+    },
     // Jika ingin tambah judul komik baru (misal: Naruto), tinggal taruh di bawah sini koma (,)
 ];
