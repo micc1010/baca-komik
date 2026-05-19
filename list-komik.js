3bd0bdd24a1a660d425e29cc99f9a62158285c03
micc1010 <michaellong23.ml@gmail.com>
2026-05-19 13:06:46 +0700

    iklan

--- list-komik.js
+++ list-komik.js
@@ -5,7 +5,6 @@ const comics = [
         title: "One Punch Man",
         poster: "https://i.imgur.com/t9JWnT4.png",
         desc: "One Punch Man mengikuti kisah Saitama, seorang pahlawan yang bisa mengalahkan musuh hanya dengan satu pukulan. Meskipun kekuatannya luar biasa, ia merasa bosan dan kesepian karena tidak ada lawan yang menantang. Petualangannya dimulai saat ia bergabung dengan Asosiasi Pahlawan dan bertemu dengan berbagai pahlawan dan monster yang unik.",
-        direct: "https://google.com", // Link Adsterra Anda
         totalChapters: 2
     },
     {
@@ -13,7 +12,6 @@ const comics = [
         title: "Solo Leveling",
         poster: "https://i.imgur.com/xKVmzM3.png",
         desc: "Di dunia di mana para pemburu bertarung melawan makhluk dari dunia lain melalui portal misterius, Sung Jinwoo adalah pemburu dengan kekuatan yang sangat lemah. Perjalanannya untuk menjadi pemburu terkuat membawa banyak tantangan serta intrik, termasuk menghadapi organisasi rahasia.",
-        direct: "https://google.com", // Link Adsterra Anda
         totalChapters: 180
     }
 ];
